c6855f31a3764ce24ae1e92f33f467337ddcad1c
Yeom JaeHeon <121541876+Y-jae-heon@users.noreply.github.com>
2024-06-28 12:02:42 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -1,34 +1,17 @@
 function solution(bandage, health, attacks) {
-  const endTime = Math.max(...attacks.map((item) => item[0]));
-  const attackInfo = new Map();
+  const [time, recovery, bonusRecovery] = bandage;
   let curHealth = health;
-  let successTime = 0;
 
-  attacks.forEach((el) => {
-    attackInfo.set(el[0], el[1]);
-  });
+  let curAttack = 0;
+  for (const [attackTime, damage] of attacks) {
+    const timeDiff = attackTime - curAttack - 1;
+    curHealth +=
+      timeDiff * recovery + Math.floor(timeDiff / time) * bonusRecovery;
 
-  for (let i = 0; i <= endTime; i++) {
-    const find = attackInfo.get(i);
-    if (find) {
-      curHealth -= find;
-      successTime = 0;
-      if(curHealth <= 0) return -1;
-    } else {
-      curHealth += bandage[1];
-      successTime++;
-      if (successTime == bandage[0]) {
-        curHealth += bandage[2];
-        successTime = 0;
-      }
-      if (curHealth > health) {
-        curHealth = health;
-      }
-    }
-  }
-
-  if (curHealth <= 0) {
-    return -1;
+    if (curHealth > health) curHealth = health;
+    curHealth -= damage;
+    if (curHealth <= 0) return -1;
+    curAttack = attackTime;
   }
 
   return curHealth;
